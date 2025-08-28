8564051172d6965b6ad500c1b167bf7ff5e3265b
Guilherme Diniz <guilhermeortizdiniz@gmail.com>
2025-08-28 10:59:10 -0300

    refactor(core): resolve circular imports

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from flask import Flask, jsonify, redirect, url_for, request
+from flask import Flask
 from app.core.config import settings as default_settings
 from app.core.extensions import db, migrate, jwt, cache
-from app.web.routes import main_bp
-from app.api.comment_routes import api_bp
-from app.api.auth_routes import auth_bp
-from app.cli import register_cli_commands
-from app.utils import format_datetime_local
-from app.celery_app import init_celery
+from app.celery_app import celery_app, init_celery
 
 def create_app(settings_override=None):
     app = Flask(__name__, template_folder='app/web/templates')
@@ -15,33 +10,33 @@ def create_app(settings_override=None):
         app.config.from_object(settings_override)
     else:
         app.config.from_object(default_settings)
-
+    
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
     cache.init_app(app)
     init_celery(app)
 
-    @jwt.unauthorized_loader
-    def unauthorized_callback(reason):
-        if request.blueprint == 'api':
-            return jsonify(msg="Missing or invalid token"), 401
-        return redirect(url_for('main.login_page'))
-
+    from app.utils import format_datetime_local
     app.jinja_env.filters['localdatetime'] = format_datetime_local
+    
+    with app.app_context():
+        from app.models import User, Comment
+        from app.web.routes import main_bp
+        from app.api.comment_routes import api_bp
+        from app.api.auth_routes import auth_bp
+        from app.cli import register_cli_commands
 
-    from app.models import User, Comment
+        app.register_blueprint(api_bp)
+        app.register_blueprint(main_bp)
+        app.register_blueprint(auth_bp)
 
-    app.register_blueprint(api_bp)
-    app.register_blueprint(main_bp)
-    app.register_blueprint(auth_bp)
-
-    register_cli_commands(app)
+        register_cli_commands(app)
 
     @app.route("/healthcheck")
     def healthcheck():
         return {"status": "ok"}, 200
-
+    
     return app
 
 app = create_app()
